b7d49b3feb46bdbbde06e8e32f42e238323062aa
Zalman <moishkugel@gmail.com>
2022-09-22 15:27:44 -0400

    have basic weather app working

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const button = document.getElementById('search')
-    let forecast = document.querySelector('#weather-forecast')
-    console.log(forecast)
+    const forecast = document.querySelector('#weather-forecast')
+    const unit = document.getElementById('unit').value
     let longitude
     let latitude
     //weatherImg is used as icon and changes depending on weather
     //value of each day object
     let weatherImg
 
+    // button.addEventListener('click', fetchCoordinates)
+
     const celciusToFarenheit = function(num){
+        if(unit === 'celcius'){
+            return num
+        } else{
         return num * 1.8 + 32
+        }
     }
 
     const removeAllChildren = function(parent) {
@@ -17,7 +23,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
             parent.removeChild(parent.firstChild);
         }
     }
-
+    
     
     const determineIcon = function(day) {
         const daysWeather = day.weather
@@ -52,34 +58,42 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
         
     }
+
+    const addPlaceName = function(city, state){
+        const h2 = document.createElement('h2');
+        h2.innerText = `${city}, ${state}`
+        forecast.appendChild(h2)
+    }
     
     const renderDaysWeather = function(day){
+        const year = day.date.toString().slice(0, 4)
         const dayOfMonth = day.date.toString().slice(6)
         const month = day.date.toString().slice(4, 6)
+        const date = new Date(`${year}-${month}-${dayOfMonth}`).toString().split(' ').slice(0, 3).join(' ');
         const minTemp = Math.round(celciusToFarenheit(day.temp2m.min)) 
         const maxTemp = Math.round(celciusToFarenheit(day.temp2m.max))
         const div = document.createElement('div')
         div.className = 'weather-card'
-        const date = document.createElement('p')
-        date.className = 'date'
-        date.innerText = `${month}/${dayOfMonth}`
+        const dateP = document.createElement('p')
+        dateP.className = 'date'
+        dateP.innerText = date
         const weatherIcon = document.createElement('img')
         weatherIcon.className = 'weather-icon'
         weatherIcon.src = weatherImg
         const high = document.createElement('p')
+        high.className = 'temp'
         high.innerText = `High: ${maxTemp}`   
         const low = document.createElement('p')
+        low.className = 'temp'
         low.innerText = `Low: ${minTemp}`
   
-        div.appendChild(date)
+        div.appendChild(dateP)
         div.appendChild(weatherIcon)    
         div.appendChild(low)
         div.appendChild(high)
 
         forecast.appendChild(div)
         
-       
-        console.log(div)
     }
 
    
@@ -87,9 +101,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
         fetch(`http://www.7timer.info/bin/api.pl?lon=${longitude}&lat=${latitude}&product=civillight&unit=imperial&output=json`)
         .then(res => res.json())
         .then(data => {
-            weekForecast = data.dataseries
+            console.log(data)
+            const weekForecast = data.dataseries
             weekForecast.forEach(day =>{
-                console.log(day)
                 determineIcon(day)
                 renderDaysWeather(day)
             } )
@@ -99,17 +113,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const fetchCoordinates = function(e){
         removeAllChildren(forecast)
         e.preventDefault()
-        const country = document.querySelector('#country').value
         const zip = document.querySelector('#zip').value
-        fetch(`https://api.zippopotam.us/${country}/${zip}`)
-        .then(res => res.json())
-        .then(data => {
-            longitude = data.places[0].longitude
-            latitude = data.places[0].latitude
-            console.log(longitude)
-            console.log(latitude)
-        })
-        fetchWeather()
+        if (zip === ""){
+            alert('Please enter valid country and zip')
+        }
+            fetch(`https://api.zippopotam.us/us/${zip}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                longitude = data.places[0].longitude
+                latitude = data.places[0].latitude
+                addPlaceName(data.places[0]['place name'], data.places[0]['state abbreviation'])
+                fetchWeather()
+            })
+        
     }
 
 
